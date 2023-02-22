6eb270449e5a0e299b574b5cc23765a0cc4ed689
Myusuf04 <nurmurzayevmuhammad@gmail.com>
2023-02-22 22:05:25 +0500

    JS first lesson

---
+++ index.js
@@ -0,0 +1,15 @@
+// let obj={};
+// function name(params) {
+    
+// }
+// let ar=[]
+// let num=1;
+// let str="salom"
+// let str;
+// console.log(str);
+// let bool=true;
+// let sym=Symbol();
+// let n=null;
+// let undef=undefined
+// let big=3n;
+// console.log(3n+1n);
